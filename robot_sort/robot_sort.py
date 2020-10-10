5116bf3b02549cfcaa3fa2833b6549c64d260d9a
Erik <spamfactorx01@gmail.com>
2020-10-09 21:06:57 -0700

    insertion sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -3,11 +3,11 @@ class SortingRobot:
         """
         SortingRobot takes a list and sorts it.
         """
-        self._list = l          # The list the robot is tasked with sorting
-        self._item = None       # The item the robot is holding
-        self._position = 0      # The list position the robot is at
-        self._light = "OFF"     # The state of the robot's light
-        self._time = 0          # A time counter (stretch)
+        self._list = l  # The list the robot is tasked with sorting
+        self._item = None  # The item the robot is holding
+        self._position = 0  # The list position the robot is at
+        self._light = "OFF"  # The state of the robot's light
+        self._time = 0  # A time counter (stretch)
 
     def can_move_right(self):
         """
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -98,9 +100,45 @@ class SortingRobot:
         """
         # Fill this out
         # self._bubble_sort()
-        self.selection_sort()
+        # self._selection_sort()
+        self._insertion_sort()
 
-    def selection_sort(self):
+    def _insertion_sort(self):
+        ## Times for Stretch Goal
+        # 558
+        # 7283
+        # 8272
+
+        while self.can_move_right():
+            self.move_right()
+            self.swap_item()
+
+            # insert into position:
+            while self.can_move_left() and not self.light_is_on():
+                self.move_left()
+
+                # Position found
+                if self.compare_item() > 0:
+                    self.set_light_on()
+                    self.move_right()
+                    self.swap_item()
+
+            # Element is smaller than all other sorted elements. Insert at front.
+            if not self.light_is_on():
+                self.swap_item()
+
+            # shift all sorted elements to the right
+            while self.compare_item() is not None:
+                self.move_right()
+                self.swap_item()
+
+            self.set_light_off()
+
+    def _selection_sort(self):
+        ## Times for Stretch Goal
+        # 820
+        # 12465
+        # 12480
 
         self.set_light_on()
 
@@ -133,10 +171,10 @@ class SortingRobot:
                 else:
                     break
 
-
     def _bubble_sort(self):
-    ## Implementation of Bubble sort
-    ## Passes main tests
+        ## Implementation of Bubble sort
+        ## Passes main tests
+        # Failes stretch goal, too many swaps.
 
         self.set_light_on()
         while self.light_is_on():
@@ -159,14 +197,112 @@ class SortingRobot:
                 self.move_left()
 
 
-
-
-
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [
+        15,
+        41,
+        58,
+        49,
+        26,
+        4,
+        28,
+        8,
+        61,
+        60,
+        65,
+        21,
+        78,
+        14,
+        35,
+        90,
+        54,
+        5,
+        0,
+        87,
+        82,
+        96,
+        43,
+        92,
+        62,
+        97,
+        69,
+        94,
+        99,
+        93,
+        76,
+        47,
+        2,
+        88,
+        51,
+        40,
+        95,
+        6,
+        23,
+        81,
+        30,
+        19,
+        25,
+        91,
+        18,
+        68,
+        71,
+        9,
+        66,
+        1,
+        45,
+        33,
+        3,
+        72,
+        16,
+        85,
+        27,
+        59,
+        64,
+        39,
+        32,
+        24,
+        38,
+        84,
+        44,
+        80,
+        11,
+        73,
+        42,
+        20,
+        10,
+        29,
+        22,
+        98,
+        17,
+        48,
+        52,
+        67,
+        53,
+        74,
+        77,
+        37,
+        63,
+        31,
+        7,
+        75,
+        36,
+        89,
+        70,
+        34,
+        79,
+        83,
+        13,
+        57,
+        86,
+        12,
+        56,
+        50,
+        55,
+        46,
+    ]
 
     robot = SortingRobot(l)
 
