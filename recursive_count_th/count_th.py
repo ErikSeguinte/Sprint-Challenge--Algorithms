968003d747ba09de0f59fa7d1d37f91a725d1c0f
Erik <spamfactorx01@gmail.com>
2020-10-09 18:57:39 -0700

    completes test 2

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,7 +5,36 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
 
-    if len(word) == 0:
+    # blank string
+    if len(word) <= 0 :
         return 0
 
-    pass
+    # String of one character cannot contain "th"
+    if len(word) == 1:
+        return 0
+
+    # String of 2 characters may contain "th"
+    if len(word) == 2:
+        if word == "th":
+            return 1
+
+    # If word contains more than 2 letters, break word in half and recurse into them
+    l = len(word) //2
+    front = word[:l]
+    back = word[l:]
+    th = 0
+    th += count_th(front)
+    th =+ count_th(back)
+
+    # after breaking, new "th" may be found at the border of front and back
+    candidate = front[-1] + back[0]
+    if candidate == "th":
+        return  th + 1
+    else:
+        return th
+
+
+
+
+
+
